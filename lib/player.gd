f4fca55240f82f316f615907b63e248dab9b8462
HenriMichelon <148531229+HenriMichelon@users.noreply.github.com>
2024-02-23 15:39:21 +0100

    clean up

--- lib/player.gd
+++ lib/player.gd
@@ -9,14 +9,14 @@ const ANIM_WALKING = "walking"
 const ANIM_RUNNING = "running"
 
 @export var camera_pivot:Node3D
+
 @onready var anim:AnimationPlayer = $AnimationPlayer 
+@onready var character:Node3D = $Character
 
 const walking_speed:float = 7.0
 const running_speed:float = 14.0
-const walking_jump_impulse:float = 20.0
 
 var camera:IsometricCamera
-var character:Node3D
 
 # for move_and_slide()
 var speed:float = 0.0
@@ -28,9 +28,6 @@ var current_view:int = 0
 # player movement signaled
 var signaled:bool = false
 
-# running animation playing
-var running:bool = false
-
 const directions = {
 	"forward" : 	[  { 'x':  1, 'z': -1 },  { 'x':  1, 'z':  1 },  { 'x': -1, 'z':  1 },  { 'x': -1, 'z': -1 } ],
 	"left" : 		[  { 'x': -1, 'z': -1 },  { 'x':  1, 'z': -1 },  { 'x':  1, 'z':  1 },  { 'x': -1, 'z':  1 } ],
@@ -39,13 +36,11 @@ const directions = {
 }
 
 func _ready():
-	character = get_node("Character")
 	camera = camera_pivot.get_node("Camera")
 	camera.connect("view_rotate", _on_view_rotate)
 
 func _physics_process(delta):
 	var on_floor = is_on_floor_only() 
-	var no_jump = false
 	var direction = Vector3.ZERO
 	if Input.is_action_pressed("player_right"):
 		direction.x += directions["right"][current_view].x
@@ -66,13 +61,10 @@ func _physics_process(delta):
 	else:
 		target_velocity.y = 0
 		signaled = false
-		running = false
 		stop_moving.emit()
 		anim.play(ANIM_STANDING, 0.2)
-	
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
-	
 	if not on_floor:
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
 	velocity = target_velocity
@@ -82,17 +74,9 @@ func _physics_process(delta):
 
 func _run_or_walk(delta):
 	if Input.is_action_pressed("modifier"):
-		GameState.player_state.endurance -= 2
-		if (GameState.player_state.endurance > 0):
-			if (not running):
-				speed = running_speed
-				anim.play(ANIM_RUNNING, 0.1)
-				running = true
-		else:
-			speed = walking_speed
-			anim.play(ANIM_WALKING, 0.1)
+		speed = running_speed
+		anim.play(ANIM_RUNNING, 0.1)
 	else:
-		running = false
 		speed = walking_speed
 		anim.play(ANIM_WALKING, 0.1)
 	moving.emit()
@@ -108,22 +92,6 @@ func _update_camera():
 	if (!signaled) :
 		start_moving.emit()
 		signaled = true
-
-func look_to(target:Vector2):
-	var ray_query = PhysicsRayQueryParameters3D.new()
-	ray_query.collision_mask = 0x1
-	ray_query.from = camera.project_ray_origin(target)
-	ray_query.to = ray_query.from + camera.project_ray_normal(target) * 1000
-	var iray = get_world_3d().direct_space_state.intersect_ray(ray_query)
-	if (iray.size() > 0):
-		var pos = iray.position
-		pos.y = position.y
-		look_at(pos)
-
-func _look_at(node:Node3D):
-	var pos = node.global_position
-	pos.y = position.y
-	look_at(pos)
 
 func _on_view_rotate(view:int):
 	current_view = view
